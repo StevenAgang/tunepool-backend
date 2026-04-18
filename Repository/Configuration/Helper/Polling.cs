eec24095895d35616558d4c44c4fb45fcc4ee4a3
StevenAgang <stevenagang06@gmail.com>
2026-04-18 20:30:38 +0800

    dockerize

--- Repository/Configuration/Helper/Polling.cs
+++ Repository/Configuration/Helper/Polling.cs
@@ -13,7 +13,7 @@ namespace tunepool.Repository.Configuration.Helper
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _timer = new Timer(async _ => await RunWeekly(cancellationToken),  null, TimeSpan.FromDays(1), TimeSpan.FromDays(1));
+            _timer = new Timer(async _ => await RunWeekly(cancellationToken),  null, TimeSpan.FromMinutes(5), TimeSpan.FromDays(1));
             return Task.CompletedTask;
         }
 
